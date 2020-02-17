8175ef9a7bfc69b220925c34379ca76f0ad8f3b0
Robel Kahsay <robel.kahsay.rk@gmail.com>
2020-02-17 21:44:37 +0100

    17.02.200

---
+++ router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("./controllers/userController");
+const postController = require('./controllers/postController')
+
+// User related routes
+router.get("/", userController.home);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+
+// Profile related routes
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
+
+//Post related routes
+router.get("/create-post",userController.mustbeLoggedIn, postController.viewCreateScreen);
+router.post("/create-post", userController.mustbeLoggedIn, postController.create)
+router.get("/post/:id", postController.viewSingle)
+router.get('/post/:id/edit', postController.viewEditScreen)
+router.post('/post/:id/edit', postController.edit)
+
+module.exports = router;
